69da530f77864cf7d6262cd0a17fa21f44ccbc25
hwangby912 <quddbs912@naver.com>
2021-12-13 23:41:30 +0900

    Business Component Practice 2

---
+++ BoardService/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.springbook.biz.user.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDAO UserDAO;
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.UserDAO = userDAO;
+	}
+
+	public UserVO getUser(UserVO vo) {
+		return UserDAO.getUser(vo);
+	}
+}
